bcc2b50660fd48d8876fcd9fe63ad8149b7dc4b0
Shuvo Saha <sahashuvo@gmail.com>
2021-09-29 01:17:39 +0600

    Print Error Message String on Text View

--- app/src/main/java/com/example/web3jtransactiontest/MainActivity.java
+++ app/src/main/java/com/example/web3jtransactiontest/MainActivity.java
@@ -20,18 +20,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView tv = findViewById(R.id.tv1);
 
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-
         StrictMode.setThreadPolicy(policy);
 
         Web3j web3j = Web3j.build(new HttpService("https://ropsten.infura.io/v3/71cf9f88c5b54962a394d80890e41d5b"));
         try{
             String from = web3j.ethGetTransactionByHash("0x91502b175eb3e4b9267e46ff15b36b5342b9cf0709292286a07fb10bba11f7c4").send().getTransaction().get().getFrom();
-            TextView tv = findViewById(R.id.tv1);
             tv.setText(from);
             Log.d("AAA","From: "+from);
         }catch (Exception e){
+            tv.setText(e.toString());
             Log.e("AAA",e.toString());
         }
 
